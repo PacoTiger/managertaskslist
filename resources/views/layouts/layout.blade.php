95788cdb8b733b771e09e9582dbe65127a354236
Francisco Padilla Muñoz <padilla.fco@gmail.com>
2017-09-18 13:45:24 +0100

    added footer

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -14,6 +14,8 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="{{ asset('css/minefooter.css') }}" rel="stylesheet">
+
 
     <style>
         body {
@@ -73,6 +75,12 @@
     </nav>
 
     @yield('content')
+
+    <footer>
+
+        <p class="text-muted credit">Example courtesy <a href="https://www.linkedin.com/in/pacotiger/en/">Francisco Padilla</a></p>
+
+    </footer>
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
